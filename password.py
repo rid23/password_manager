430a8c1e7f77a890beb6266316f2cb9fe8f3502e
rid23 <rid.bhattacharyya23@gmail.com>
2020-11-30 19:52:42 +0530

    created the idd function , added exception handling in db_insert

--- password.py
+++ password.py
@@ -17,26 +17,64 @@ def window():
 	return f,root
 
 def db_insert(idd,nm,pwd):
-	string = "INSERT INTO pass(id,name,password) VALUES (%s,%s,%s)"
-	values = (idd,nm,pwd)
-	con,cur = db_conn()
-	cur.execute(string,values)
-	con.commit()
+	try:
+		string = "INSERT INTO pass(id,name,password) VALUES (%s,%s,%s)"
+		values = (idd,nm,pwd)
+		con,cur = db_conn()
+		cur.execute(string,values)
+		con.commit()
+		cur.close()
+		con.close()
+		print("Upload Complete Bro")
+	except Exception as e:print(e)
+
+
+
+
+def click(event):
+	global enter1,enter2
+	enter1=str(e1.get())
+	enter2=str(e2.get())
+	print(enter1)
+	print(enter2)
+	db_insert(idd(),enter1,enter2)
+
+def idd():
+	con,cur=db_conn()
+	cur.execute("SELECT id FROM pass")
+	itm = cur.fetchall()
 	cur.close()
 	con.close()
-	print("Upload Complete Bro")
+	idd = int(itm[-1][0])+1
+	return idd
 
 
-
-def click():
-	string = "SELECT id FROM pass"
-	con,cur = db_conn()
-	cur.execute(string)
-	#Items from the id column of pass table
-	itm=cur.fetchall()
-	con.commit()
-	cur.close()
-	con.close()
-	db_insert(int(itm[-1][0])+1,str(e1.get()),str(e2.get()))
 
 frame,win = window()
+
+#creating the label,entry,button widgets
+label = Label(frame,text="USERNAME")
+label2=Label(frame,text="P@$$WORD")
+e1=Entry(frame,width=8)
+e2=Entry(frame,width=8)
+btn=Button(frame,text="Correct",width=6,activebackground="black",activeforeground="white")
+
+#Packing all the widgets
+label.pack()
+e1.pack()
+label2.pack()
+e2.pack()
+
+btn.pack()
+btn.bind("<Button-1>",click)
+
+
+win.mainloop()
+
+
+
+
+
+
+
+
